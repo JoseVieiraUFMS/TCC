fdd110936be329352a0d280e67630a6f6067afb6
JoseVieiraUFMS <51810554+JoseVieiraUFMS@users.noreply.github.com>
2019-06-14 01:02:58 -0300

    Add files via upload

---
+++ programa_Mega_Help.c
@@ -0,0 +1,24 @@
+#pragma config(Sensor, S2,     ,               sensorEV3_Infravermelho,modeEV3Proximidade_Proximidade)
+void elevador(){
+	setMotorSpeed(MotorA, 30);
+	wait1Msec(12000);
+}
+void chute(){
+	setMotorSpeed(MotorB, 100);
+	wait1Msec(400);
+}
+void esteira(){
+		setMotorSpeed(MotorC, 30);
+	wait1Msec(10000);
+}
+
+task main(){
+	while(true){
+		if(SensorValue(S2)<10){
+			elevador();
+			chute();
+			esteira();
+		}
+	}
+	
+}
